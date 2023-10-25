29bb4b2a44252d5b8355da1d796b81097992b395
mporada259449 <259449@student.pwr.edu.pl>
2023-10-25 19:39:34 +0000

    redis added to app

--- calculator/views.py
+++ calculator/views.py
@@ -1,24 +1,37 @@
 from django.shortcuts import render
 from django.views import View
 from calculator.calculator import Calculator
+from django.core.cache import cache
+
 class Calc(View):
     template = "calc.html"
 
     def get(self, request):
+        cache.clear()
         return render(request, self.template)
     
     def post(self, request):
-        num1 = int(request.POST.get("first"))
-        num2 = int(request.POST.get("second"))
+        num1 = float(request.POST.get("first"))
+        num2 = float(request.POST.get("second"))
+        operation = request.POST.get("operation")
         calc = Calculator(num1, num2)
-
-        if "add" in request.POST:
-            return render(request, self.template, {"result": calc.add()})
-        elif "subtract" in request.POST:
-            return render(request, self.template, {"result": calc.subtract()})
-        elif "multiply" in request.POST:
-            return render(request, self.template, {"result": calc.multiply()})
-        elif "divide" in request.POST:
-            return render(request, self.template, {"result": calc.divide()})
+        result = None
 
 
+        if operation == "add":
+            result = calc.add()
+        elif operation =="subtract":
+            result = calc.subtract()
+        elif operation == "multiply":
+            result = calc.multiply()
+        elif operation == "divide":
+            result = calc.divide()
+
+        user_results = cache.get("user_results", [])
+        print(user_results)
+        if isinstance(result, float):
+           user_results.append({"num1": num1, "num2": num2, "result": result, "operation": operation})
+           cache.set("user_results", user_results)
+
+           
+        return render(request, self.template, {"result": result, "user_results": user_results})
