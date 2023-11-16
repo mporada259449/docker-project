6158a06524269cb0c2509d362acd5de447f2a83e
mporada259449 <259449@student.pwr.edu.pl>
2023-11-16 19:12:29 +0000

    password change view

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth.views import PasswordChangeView, LoginView, LogoutView
 
 urlpatterns = [
-    path(r'auth/', include("django.contrib.auth.urls")),
-    #path(r'login/', views.Login.as_view(), name="login"),
+    path(r'auth/password_change/', PasswordChangeView.as_view(success_url = '/'), name="password_change"),
     path(r'auth/register/', views.Register.as_view(), name="register"),
+    path(r'auth/login/', LoginView.as_view(), name='login'),
+    path(r'auth/logout/', LogoutView.as_view(), name='logout')
 ]
